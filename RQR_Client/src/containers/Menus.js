b60dd9e804f400250d1bb9840c776e6da9e64b52
Stedy Yulius <stedyyulius@gmail.com>
2017-11-20 17:53:07 +0700

    add qr reader

--- RQR_Client/src/containers/Menus.js
+++ RQR_Client/src/containers/Menus.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react'
+import QRCodeScanner from 'react-native-qrcode-scanner';
 import { connect } from 'react-redux'
 import{
   View,
   StyleSheet,
-  Text
+  Text,
+  Linking,
+  TouchableOpacity
 } from 'react-native'
 
 class Menus extends Component{
@@ -11,19 +14,43 @@ class Menus extends Component{
     super(props)
     this.state={}
   }
+
+  onSuccess(e) {
+  Linking.openURL(e.data).catch(err => alert('An error occured', err));
+}
+
   render(){
     return(
-      <View>
-        <Text>
-          ini menu
-        </Text>
+      <View style={styles.container}>
+        <QRCodeScanner
+           onRead={this.onSuccess.bind(this)}
+           topContent={(
+             <Text>
+               Go to <Text style={styles.link} onPress={()=> Linking.openURL('wikipedia.org/wiki/QR_code')}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+             </Text>
+           )}
+           bottomContent={(
+             <TouchableOpacity>
+               <Text>OK. Got it!</Text>
+             </TouchableOpacity>
+           )}
+         />
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+    width: null,
+    height: null
+  },
+  link:{
+    color: 'blue'
+  }
 })
 
 const mapStateToProps = (state) =>{
