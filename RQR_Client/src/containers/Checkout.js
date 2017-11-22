234b12503366f2573eac03d8a7a2539fc2a26589
Stedy Yulius <stedyyulius@gmail.com>
2017-11-22 11:56:51 +0700

    scroll

--- RQR_Client/src/containers/Checkout.js
+++ RQR_Client/src/containers/Checkout.js
@@ -30,42 +30,44 @@ class Checkout extends Component{
   render(){
     const { navigate } = this.props.navigation
     return(
-      <View style={styles.container}>
-        <CardOrder
-          icon={'https://scontent.fsub6-3.fna.fbcdn.net/v/t1.0-1/p160x160/12096566_10153589536696427_3240500719548235275_n.jpg?oh=5772d7971a9bc39de1bf7a789cebff84&oe=5AA6AFB2'}
-          name="Felix"
-        />
-        <Button
-          title="Pay for Myself"
-          color="black"
-          onPress={()=> this.setState({isPay: true})} />
-        <View>
-          {(this.state.isPay)
-            ? <View>
-                <View style={{marginTop: 30}}>
-                  <Text style={styles.title}>
-                    Payment Method
-                  </Text>
-                  <View>
-                    <Text style={{fontWeight:'bold',borderBottomWidth:2}}>
-                      Credit Card
+      <ScrollView>
+        <View style={styles.container}>
+          <CardOrder
+            icon={'https://scontent.fsub6-3.fna.fbcdn.net/v/t1.0-1/p160x160/12096566_10153589536696427_3240500719548235275_n.jpg?oh=5772d7971a9bc39de1bf7a789cebff84&oe=5AA6AFB2'}
+            name="Felix"
+          />
+          <Button
+            title="Pay for Myself"
+            color="black"
+            onPress={()=> this.setState({isPay: true})} />
+          <View>
+            {(this.state.isPay)
+              ? <View>
+                  <View style={{marginTop: 30}}>
+                    <Text style={styles.title}>
+                      Payment Method
                     </Text>
-                    {CreditCard('https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/200px-MasterCard_early_1990s_logo.svg.png','Master Card')}
-                    {CreditCard('https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Visa.svg/1200px-Visa.svg.png','Visa')}
-                  </View>
-                  <View>
-                    <Text style={{fontWeight:'bold',borderBottomWidth:2}}>
-                      Debit Card
-                    </Text>
-                    {CreditCard('http://sp.beritasatu.com/media/images/original/20111221144834840.jpg','BCA')}
-                    {CreditCard('http://indowarta.com/wp-content/uploads/2017/05/logo.jpg','Mandiri')}
+                    <View>
+                      <Text style={{fontWeight:'bold',borderBottomWidth:2}}>
+                        Credit Card
+                      </Text>
+                      {CreditCard('https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/MasterCard_early_1990s_logo.svg/200px-MasterCard_early_1990s_logo.svg.png','Master Card')}
+                      {CreditCard('https://upload.wikimedia.org/wikipedia/commons/thumb/0/04/Visa.svg/1200px-Visa.svg.png','Visa')}
+                    </View>
+                    <View style={styles.debitCard}>
+                      <Text style={{fontWeight:'bold',borderBottomWidth:2}}>
+                        Debit Card
+                      </Text>
+                      {CreditCard('http://sp.beritasatu.com/media/images/original/20111221144834840.jpg','BCA')}
+                      {CreditCard('http://indowarta.com/wp-content/uploads/2017/05/logo.jpg','Mandiri')}
+                    </View>
                   </View>
                 </View>
-              </View>
-            : null
-          }
+              : null
+            }
+          </View>
         </View>
-      </View>
+      </ScrollView>
     )
   }
 }
@@ -80,6 +82,9 @@ const styles = StyleSheet.create({
     fontSize: 15,
     alignSelf: 'center'
   },
+  debitCard:{
+    marginTop: 15
+  }
 })
 
 const mapStateToProps = (state) =>{
