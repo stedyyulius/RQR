443f3027cf627de3580584b756021c60881f30a5
Stedy <Stedy@Felixs-MacBook-Pro-2.local>
2018-01-11 21:51:17 +0700

    user schema

---
+++ server/functions/models/User.js
@@ -0,0 +1,17 @@
+import { Schema } from 'mongoose'
+
+var UserSchema = new Schema({
+  fullname: String,
+  username: String,
+  password: String,
+  email   : String,
+  handphone: String,
+  device_id: Array,
+  last_login: Date,
+  created_at: Date,
+  updated_at: Date,
+})
+
+var User = mongoose.model('User',UserSchema)
+
+export default User
